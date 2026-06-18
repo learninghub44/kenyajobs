2f56f128c55d615eddedef79484ded95194b4132
JobsWorldwide Dev <dev@jobsworldwide.online>
2026-06-18 21:09:05 +0000

    feat: switch to DM Sans font, 18px base size for readability

--- kenyajobs/pages/_app.js
+++ kenyajobs/pages/_app.js
@@ -1,12 +1,9 @@
 // pages/_app.js
 import "@/styles/globals.css";
-import "@fontsource/space-grotesk/500.css";
-import "@fontsource/space-grotesk/600.css";
-import "@fontsource/space-grotesk/700.css";
-import "@fontsource/inter/400.css";
-import "@fontsource/inter/500.css";
-import "@fontsource/inter/600.css";
-import "@fontsource/inter/700.css";
+import "@fontsource/dm-sans/400.css";
+import "@fontsource/dm-sans/500.css";
+import "@fontsource/dm-sans/600.css";
+import "@fontsource/dm-sans/700.css";
 import "@fontsource/ibm-plex-mono/400.css";
 import "@fontsource/ibm-plex-mono/500.css";
 import Navbar from "@/components/Navbar";
